7f78442e807672d19d0369a01e9bfb41ab36ab47
benosolomon <45790644+benosolomon@users.noreply.github.com>
2025-06-26 15:13:10 +0530

    Process In/out

--- node/app.js
+++ node/app.js
@@ -1,4 +1,19 @@
-global.console.log("Hello world , welcome to Node");
+process.stdout.write("beno\n");
+process.stdout.write("Solomon");
 
-console.log(__dirname);
-console.log(__filename);
+const arr = ["Dog","Cat","Rabbit"];
+
+const output = (val= 0) => {
+    process.stdout.write(arr[val]);
+}
+
+output(2);
+
+process.stdin.on('data',data => {
+   process.stdout.write(`you wrote  ${data}`); 
+   process.exit();
+});
+
+process.on('exit',(data)=> {
+    console.log('you exited');
+})
