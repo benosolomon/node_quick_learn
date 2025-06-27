ebdf052c7ade12c4647fbf3a190c810be2c9fdb5
benosolomon <45790644+benosolomon@users.noreply.github.com>
2025-06-27 11:02:17 +0530

    html page advanced

--- node/htmlpage.js
+++ node/htmlpage.js
@@ -1,15 +1,26 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 const port = 8080;
 const host = '127.0.0.1';
 const server = http.createServer((request, response) => {
-  fs.readFile('./mods/index.html', (error, html) => {
-    response.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-    response.write(`<script>alert('hello');</script>`);
-    response.write(html);
-    response.end();
+  const base = url.parse(request.url);
+  //console.log(base);
+  const fileName = './mods' + base.pathname;
+  fs.readFile(fileName, (error, html) => {
+    if (error) {
+      response.writeHead(404, {
+        'Content-Type': 'text/html'
+      });
+      response.end(`<h1>Not Found</h1>`);
+    }
+    else {
+      response.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      response.write(html);
+      response.end();
+    }
   })
 })
 server.listen(port, host, () => {
